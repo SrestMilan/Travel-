072b18ebda022d6bcac0c4cdd867a50e31ac6feb
SrestMilan <shrestham780@gmail.com>
2023-10-10 15:41:55 +0545

    value added

--- travel-lis/src/components/Form.js
+++ travel-lis/src/components/Form.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Form() {
+  const [quantity, setQuantity] = useState(1);
   return (
     <div className="add-form">
       <h3>What do you need for your 😍 trip?</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num}>{num}</option>
+        ))}
+      </select>
     </div>
   );
 }
