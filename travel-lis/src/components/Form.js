424a8566f5575b1b69e513f3c58c3f4df35cd6db
SrestMilan <shrestham780@gmail.com>
2023-10-10 15:50:16 +0545

    state updated

--- travel-lis/src/components/Form.js
+++ travel-lis/src/components/Form.js
@@ -2,15 +2,38 @@ import React, { useState } from "react";
 
 function Form() {
   const [quantity, setQuantity] = useState(1);
+  const [description, setDescription] = useState(null);
+
+  let handleForm = (e) => {
+    e.preventDefault();
+
+    setDescription("");
+    setQuantity(1);
+
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: Date.now(),
+    };
+  };
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleForm}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num}>{num}</option>
         ))}
       </select>
-    </div>
+      <input
+        type="text"
+        placeholder="item"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>ADD</button>
+    </form>
   );
 }
 
