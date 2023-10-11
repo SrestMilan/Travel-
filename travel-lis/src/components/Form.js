c40fc3e2cfb113c58d80f5c48afba4302fd9a3f8
SrestMilan <shrestham780@gmail.com>
2023-10-11 11:59:22 +0545

    update in project

--- travel-lis/src/components/Form.js
+++ travel-lis/src/components/Form.js
@@ -1,40 +1,42 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
-function Form() {
+export default function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
-  const [description, setDescription] = useState(null);
 
-  let handleForm = (e) => {
+  function handleSubmit(e) {
     e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    onAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
-
-    const newItem = {
-      description,
-      quantity,
-      packed: false,
-      id: Date.now(),
-    };
-  };
+  }
 
   return (
-    <form className="add-form" onSubmit={handleForm}>
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num}>{num}</option>
+          <option value={num} key={num}>
+            {num}
+          </option>
         ))}
       </select>
       <input
         type="text"
-        placeholder="item"
+        placeholder="Item..."
         value={description}
         onChange={(e) => setDescription(e.target.value)}
       />
-      <button>ADD</button>
+      <button>Add</button>
     </form>
   );
 }
-
-export default Form;
--- travel-lis/src/components/Stats.js
+++ travel-lis/src/components/Stats.js
@@ -1,11 +1,22 @@
-import React from "react";
+export default function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
 
-function Stats() {
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>💼You have X items on your list, and already packed X (X%)</em>{" "}
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : ` 💼 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
-
-export default Stats;
