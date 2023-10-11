241a83e13b34c5952da614b8bd96b734d38385c5
SrestMilan <shrestham780@gmail.com>
2023-10-11 12:25:59 +0545

    previous

--- travel-lis/src/components/Form.js
+++ travel-lis/src/components/Form.js
@@ -1,42 +1,49 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-export default function Form({ onAddItems }) {
-  const [description, setDescription] = useState("");
+function Form({ onAddItems }) {
   const [quantity, setQuantity] = useState(1);
+  const [description, setDescription] = useState(null);
 
-  function handleSubmit(e) {
+  let handleForm = (e) => {
     e.preventDefault();
 
     if (!description) return;
 
-    const newItem = { description, quantity, packed: false, id: Date.now() };
+    setDescription("");
+    setQuantity(1);
+
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: Date.now(),
+    };
 
     onAddItems(newItem);
 
-    setDescription("");
-    setQuantity(1);
-  }
+    console.log(newItem);
+  };
 
   return (
-    <form className="add-form" onSubmit={handleSubmit}>
+    <form className="add-form" onSubmit={handleForm}>
       <h3>What do you need for your 😍 trip?</h3>
       <select
         value={quantity}
         onChange={(e) => setQuantity(Number(e.target.value))}
       >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num} key={num}>
-            {num}
-          </option>
+          <option value={num}>{num}</option>
         ))}
       </select>
       <input
         type="text"
-        placeholder="Item..."
+        placeholder="item"
         value={description}
         onChange={(e) => setDescription(e.target.value)}
       />
-      <button>Add</button>
+      <button>ADD</button>
     </form>
   );
 }
+
+export default Form;
